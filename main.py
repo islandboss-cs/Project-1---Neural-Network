495092b51a47f8179f353c3aec350b104064a103
elektroedde <edvinberling@gmail.com>
2025-09-09 11:45:18 +0200

    first

---
+++ main.py
@@ -0,0 +1,39 @@
+
+##! PROJECT 1 - Neural Network
+##TODO TASK 1
+
+
+
+
+
+##TODO TASK 2
+
+
+
+
+
+##TODO TASK 3
+
+
+
+
+
+##TODO TASK 4
+
+
+
+
+
+##TODO TASK 5
+
+
+
+
+
+##TODO TASK 6
+
+
+
+
+
+##TODO TASK 7

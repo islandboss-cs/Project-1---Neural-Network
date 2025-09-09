f06ead63e64ea143baec1f8097b462b9937c8753
elektroedde <edvinberling@gmail.com>
2025-09-09 11:48:59 +0200

    f

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
 
 ##! PROJECT 1 - Neural Network
+##? Placeholder task assignments
+
 ##TODO TASK 1
+##? Main responsibility: Rafael
 
 
 
 
 
 ##TODO TASK 2
+##? Main responsibility: Las
 
 
 
 
 
 ##TODO TASK 3
+##? Main responsibility: Edvin
 
 
 
 
 
 ##TODO TASK 4
+##? Main responsibility: Martin
 
 
 
 
 
 ##TODO TASK 5
+##? Main responsibility: Rafael
 
 
 
 
 
 ##TODO TASK 6
+##? Main responsibility: Las
 
 
 
 
 
 ##TODO TASK 7
+##? Main responsibility: Edvin
